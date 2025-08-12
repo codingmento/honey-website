90ccdf34286b7d0c4a635ecf7e43e275640de48f
coding.asmaa <asmaa.adass.a@gmail.com>
2025-08-12 23:11:32 +0300

    Update honey.js

--- honey.js
+++ honey.js
@@ -33,17 +33,7 @@ function SearchData(searchQuery) {
     }
 }
 
-let navigation = document.querySelector('.navigation');
-document.querySelector('#menu-btn').onclick = () =>{
-    navigation.classList.toggle('active');
-  
-}
 
-
-window.onscroll = ()=>{
-    navigation.classList.remove('active');
-    
-}
 
 
 
